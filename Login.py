4a0ca54e14a19878ede71ef8f0e2eb4060d4a479
Sheikh M Sahil <55999445+Ashura-Sheikh@users.noreply.github.com>
2020-10-03 16:40:51 +0100

    comments

--- Login.py
+++ Login.py
@@ -1,18 +1,21 @@
 from tkinter import *
 from tkinter import Label
 
-# test
+
+# Main Window
 window = Tk()
 window.title("Welcome to Ashura")
 window.geometry("500x500")
 window.configure(background="black")
 
+# Functions to Destroy secondary windows
 def delete():
     window1.destroy()
 
 def delete1():
     window2.destroy()
 
+# Fuction for Error in inouting User Data
 def error():
     global window1
     window1 = Toplevel(window)
@@ -22,6 +25,7 @@ def error():
     Label(window1,text="All Fields Mandatory *", fg="red", bg="black").pack()
     Button(window1, text="OK", command=delete, fg="Green", bg="Black").pack()
 
+# Function for succesful Registration
 def success():
     global window2
     window2 = Toplevel(window)
@@ -31,9 +35,14 @@ def success():
     Label(window2, text= "Welcome to Ashura", fg="Green", bg="Black").pack()
     Button(window2, text="OK", command=delete1,fg="Green", bg="Black").pack()
 
+
+
+# Register function
 def register():
     user_text = user_name.get()
     password_text = password.get()
+
+
 
     if user_text == "":
         error()
@@ -43,20 +52,25 @@ def register():
         success()
 
 
-
+# Heading
 heading = Label(text="Login Required For Ashura Access..", fg="Green", bg="black").pack()
 
-
+# Labels
 Label(text="User Name * ", fg="Green", bg="Black").place(x= 15, y= 70)
 Label(text="Password *", fg="Green", bg="Black").place(x= 15, y= 140)
 
+# USer info
 user_name = StringVar()
 password = StringVar()
 
-
+# Entries for Info
 Entry(window, textvariable=user_name).place(x=15, y=90)
 Entry(window, textvariable=password).place(x=15, y=160)
 
+
+# Submit Button
 Button(window, text="Submit", width="7",fg="Green", bg="Black", command= register).place(x=15, y=190)
 
+
+# Looping through UI 
 window.mainloop()
